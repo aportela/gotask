69f8a04343136f45176587100eb92a18b07c2625
alex <766f6964+github@gmail.com>
2026-04-22 21:55:43 +0200

    build production into golang public path, also split vendor assets into separated chunks

--- vite-vue-ts-frontend/vite.config.ts
+++ vite-vue-ts-frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    outDir: path.resolve(__dirname, "../golang-backend/public"), // 👈 muy común con Go
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
