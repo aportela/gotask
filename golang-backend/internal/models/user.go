290ab6394299ed1d9d4ceeabdd281c215cb7f48d
alex <766f6964+github@gmail.com>
2026-04-23 14:44:47 +0200

    added last update field

--- golang-backend/internal/models/user.go
+++ golang-backend/internal/models/user.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	ID        int    `json:"id"`
-	Email     string `json:"email"`
-	Name      string `json:"name"`
-	CreatedAt int64  `json:"createdAt"`
+	ID           int    `json:"id"`
+	Email        string `json:"email"`
+	Name         string `json:"name"`
+	CreatedAt    int64  `json:"createdAt"`
+	LastUpdateAt int64  `json:"lastUpdateAt"`
 }
