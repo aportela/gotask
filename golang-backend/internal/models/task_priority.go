ff31a95e6fcf1ae69e54d1261b3ed03297cbe87e
alex <766f6964+github@gmail.com>
2026-04-23 21:14:31 +0200

    property color refactored

--- golang-backend/internal/models/task_priority.go
+++ golang-backend/internal/models/task_priority.go
@@ -1,8 +1,8 @@
 package models
 
 type TaskPriority struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Index int    `json:"index"`
-	Color string `json:"color"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Index    int    `json:"index"`
+	HexColor string `json:"hexColor"`
 }
