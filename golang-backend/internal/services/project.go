dd73aec912104b0a9f6cc53f1757fb44c4513bad
alex <766f6964+github@gmail.com>
2026-04-23 22:04:41 +0200

    added context

--- golang-backend/internal/services/project.go
+++ golang-backend/internal/services/project.go
@@ -1,22 +1,26 @@
 package services
 
 import (
+	"context"
+
 	"github.com/aportela/gotask/internal/models"
 	"github.com/aportela/gotask/internal/repositories"
 )
 
 type ProjectService struct {
-	repo *repositories.ProjectRepository
+	repository *repositories.ProjectRepository
 }
 
-func NewProjectService(repo *repositories.ProjectRepository) *ProjectService {
+func NewProjectService(repository *repositories.ProjectRepository) *ProjectService {
 	return &ProjectService{
-		repo: repo,
+		repository: repository,
 	}
 }
-func (s *ProjectService) Get(id string) (models.Project, error) {
-	return s.repo.GetByID(id)
+
+func (s *ProjectService) GetProject(ctx context.Context, id string) (models.Project, error) {
+	return s.repository.Get(ctx, id)
 }
-func (s *ProjectService) Search(id string) ([]models.Project, error) {
-	return s.repo.Search()
+
+func (s *ProjectService) SearchProjects(ctx context.Context) ([]models.Project, error) {
+	return s.repository.Search(ctx)
 }
