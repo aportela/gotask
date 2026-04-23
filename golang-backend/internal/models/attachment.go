40a0daca82876bf590a50a1f202c52146ed43cec
alex <766f6964+github@gmail.com>
2026-04-23 21:14:42 +0200

    added creator property

--- golang-backend/internal/models/attachment.go
+++ golang-backend/internal/models/attachment.go
@@ -4,5 +4,6 @@ type Attachment struct {
 	ID        string `json:"id"`
 	FileName  string `json:"fileName"`
 	SizeBytes int64  `json:"sizeBytes"`
+	CreatedBy User   `json:"createdBy"`
 	CreatedAt int64  `json:"createdAt"`
 }
