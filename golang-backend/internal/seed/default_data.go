32f1aed17cf85ad5fe92f05bb24d7949990327ca
alex <766f6964+github@gmail.com>
2026-04-24 19:18:28 +0200

    new default data creation function

---
+++ golang-backend/internal/seed/default_data.go
@@ -0,0 +1,34 @@
+package seed
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aportela/gotask/internal/database"
+	"github.com/aportela/gotask/internal/models"
+	"github.com/aportela/gotask/internal/repositories"
+	"github.com/aportela/gotask/internal/services"
+	"github.com/aportela/gotask/internal/utils"
+	"github.com/gofrs/uuid"
+)
+
+func CreateDefaultData(db database.Database) {
+	userRepository := repositories.NewUserRepository(db)
+	userService := services.NewUserService(userRepository)
+
+	userID := func() string { u, _ := uuid.NewV7(); return u.String() }()
+	password := "secret"
+	err := userService.AddUser(context.Background(), models.User{
+		UserBase: models.UserBase{
+			ID:   userID,
+			Name: "administrator",
+		},
+		Email:        "admin@localhost",
+		Password:     &password,
+		CreatedAt:    utils.CurrentTimestamp(),
+		LastUpdateAt: nil,
+	})
+	if err != nil {
+		fmt.Printf("Error creating user %s\n", err.Error())
+	}
+}
