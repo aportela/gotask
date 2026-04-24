9c02dd0205c62b6ceb78fb8495a627a292cf7072
alex <766f6964+github@gmail.com>
2026-04-24 09:01:55 +0200

    added user service

---
+++ golang-backend/internal/services/user.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"context"
+
+	"github.com/aportela/gotask/internal/models"
+	"github.com/aportela/gotask/internal/repositories"
+)
+
+type UserService struct {
+	repository *repositories.UserRepository
+}
+
+func NewUserService(repository *repositories.UserRepository) *UserService {
+	return &UserService{
+		repository: repository,
+	}
+}
+
+func (s *UserService) AddUser(ctx context.Context, user models.User) error {
+	return s.repository.Add(ctx, user)
+}
+
+func (s *UserService) UpdateUser(ctx context.Context, user models.User) error {
+	return s.repository.Update(ctx, user)
+}
+
+func (s *UserService) DeleteUser(ctx context.Context, id string) error {
+	return s.repository.Delete(ctx, id)
+}
+
+func (s *UserService) SearchUsers(ctx context.Context) ([]models.User, error) {
+	return s.repository.Search(ctx)
+}
