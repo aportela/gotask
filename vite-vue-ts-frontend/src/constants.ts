7e86f5ace7fcfddf9daa955afd3c0a05aa2ac270
alex <766f6964+github@gmail.com>
2026-04-27 23:34:57 +0200

    path fixed

--- vite-vue-ts-frontend/src/constants.ts
+++ vite-vue-ts-frontend/src/constants.ts
@@ -2,6 +2,6 @@ export const GITHUB_PROJECT_URL = "http://github.com/aportela/doneo";
 // this (prefix/namespace) is for preventing sharing keys on web sites hosted on same url
 export const LOCAL_STORAGE_NAMESPACE = "doneo#";
 
-export const SERVER_API_BASE_PATH = "api";
+export const SERVER_API_BASE_PATH = "/api";
 
 export const DEFAULT_LOCALE = "en-US";
