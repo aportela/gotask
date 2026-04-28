3c962cb175999de4a2c6b364c9e9f30b7627760c
alex <766f6964+github@gmail.com>
2026-04-28 15:51:53 +0200

    added layout for 404 pages

--- vite-vue-ts-frontend/src/layouts/ErrorNotFoundLayout.vue
+++ vite-vue-ts-frontend/src/layouts/ErrorNotFoundLayout.vue
@@ -1,6 +1,42 @@
+<script setup lang="ts">
+    import { useRouter } from 'vue-router'
+
+    import { NResult, NSpace, NButton } from 'naive-ui'
+
+    const router = useRouter()
+
+    const goHome = () => {
+        router.push('/')
+    }
+
+    const goBack = () => {
+        router.back()
+    }
+</script>
+
 <template>
-    <router-view />
+    <div class="not-found">
+        <n-result status="404" title="404 Not Found" description="We can't find what you're looking for." size="huge">
+            <template #footer>
+                <n-space justify="space-around">
+                    <n-button type="primary" @click="goHome">
+                        Go home
+                    </n-button>
+                    <n-button tertiary @click="goBack">
+                        Go back
+                    </n-button>
+                </n-space>
+            </template>
+        </n-result>
+    </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<style lang="css" scoped>
+    .not-found {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 24px;
+    }
+</style>
