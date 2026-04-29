ff4a46e0de0a29691a2ddd0f35f5a19aba5d499e
alex <766f6964+github@gmail.com>
2026-04-29 21:56:45 +0200

    new button for toggle fluid layout

---
+++ vite-vue-ts-frontend/src/components/buttons/SwitchFluidLayoutButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { NButton } from 'naive-ui';
+    import { IconLayout } from '@tabler/icons-vue';
+    import { useUserSettingsStore } from '../../stores/userSettings';
+
+    const userSettingsStore = useUserSettingsStore();
+
+</script>
+
+<template>
+    <n-button quaternary @click="userSettingsStore.toggleFluidLayout" @mousedown.prevent>
+        <IconLayout />
+    </n-button>
+</template>
+
+<style lang="css" scoped></style>
