6c6ee25fb52f5c0478d898f3536776b380c54d92
alex <766f6964+github@gmail.com>
2026-05-04 15:00:13 +0200

    use ColoredIconSelect component

--- vite-vue-ts-frontend/src/components/selectors/ProjectTypeSelector.vue
+++ vite-vue-ts-frontend/src/components/selectors/ProjectTypeSelector.vue
@@ -1,39 +1,27 @@
 <script setup lang="ts">
-    import { h, reactive, onMounted, type CSSProperties, shallowRef, type VNodeChild } from 'vue';
+    import { reactive, shallowRef, onMounted } from 'vue';
     import { useI18n } from "vue-i18n";
-    import { NSelect, NIcon } from 'naive-ui';
     import { api } from '../../composables/api';
     import { type SearchProjectTypesResponse } from '../../types/apiResponses';
     import { type AjaxStateInterface, defaultAjaxState } from "../../types/ajaxState";
     import { type NaiveUISelectOptionWithColor } from '../../types/customNaiveUI';
-    import { IconSquareFilled } from '@tabler/icons-vue';
+    import { default as ColoredIconSelect } from './ColoredIconSelect.vue';
 
     const { t } = useI18n();
 
     interface ProjectTypeSelectorProps {
         workspaceId: string;
-        style?: string | CSSProperties;
     };
 
     const props = defineProps<ProjectTypeSelectorProps>();
 
     const emit = defineEmits(['error'])
 
-    const selectedProjectType = defineModel<string | null>('value');
+    const modelValue = defineModel<string | null>('value');
 
     const options = shallowRef<NaiveUISelectOptionWithColor[]>([]);
 
     const state: AjaxStateInterface = reactive({ ...defaultAjaxState });
-
-    const findValidProjectType = (options: NaiveUISelectOptionWithColor[], stored?: string | null) => {
-        if (!options.length) return null;
-
-        if (stored && options.some(o => o.value === stored)) {
-            return stored;
-        } else {
-            return null
-        }
-    };
 
     const onRefresh = () => {
         Object.assign(state, defaultAjaxState);
@@ -45,10 +33,6 @@
                 disabled: false,
                 color: item.hexColor,
             }));
-            selectedProjectType.value = findValidProjectType(
-                options.value,
-                props.workspaceId
-            );
         }).catch((error: any) => {
             emit("error", { error: error });
         }).finally(() => {
@@ -56,36 +40,14 @@
         });
     };
 
-    const renderLabel = (option: NaiveUISelectOptionWithColor): VNodeChild => {
-        if (option.type === 'group')
-            return `${option.label}(Cool!)`
-        return [
-            h(
-                NIcon,
-                {
-                    color: option.color,
-                    style: {
-                        verticalAlign: '-0.15em',
-                        marginRight: '4px'
-                    }
-                },
-                {
-                    default: () => h(IconSquareFilled)
-                }
-            ),
-            option.label as string
-        ]
-    }
-
     onMounted(() => {
         onRefresh();
     });
 </script>
 
 <template>
-    <n-select :placeholder="t('Project type')" v-model:value="selectedProjectType" :options="options"
-        :render-label="renderLabel" :style="style" :disabled="state.ajaxRunning"
-        :loading="state.ajaxRunning"></n-select>
+    <ColoredIconSelect :placeholder="t('Project type')" v-model:value="modelValue" :options="options"
+        :filterable="false" />
 </template>
 
 <style lang="css" scoped></style>
--- vite-vue-ts-frontend/src/components/selectors/WorkspaceSelector.vue
+++ vite-vue-ts-frontend/src/components/selectors/WorkspaceSelector.vue
@@ -1,47 +1,21 @@
 <script setup lang="ts">
-    import { h, reactive, watch, onMounted, type CSSProperties, shallowRef, type VNodeChild } from 'vue';
+    import { reactive, shallowRef, onMounted } from 'vue';
     import { useI18n } from "vue-i18n";
-    import { NSelect, NIcon } from 'naive-ui';
     import { api } from '../../composables/api';
     import { type SearchWorkspacesResponse } from '../../types/apiResponses';
     import { type AjaxStateInterface, defaultAjaxState } from "../../types/ajaxState";
     import { type NaiveUISelectOptionWithColor } from '../../types/customNaiveUI';
-    import { IconSquareFilled } from '@tabler/icons-vue';
-    import { useCurrentWorkspaceStore } from '../../stores/currentWorkspace';
+    import { default as ColoredIconSelect } from './ColoredIconSelect.vue';
 
     const { t } = useI18n();
 
-    const currentWorkspaceStore = useCurrentWorkspaceStore();
-
-    interface WorkspaceSelectorProps {
-        style?: string | CSSProperties;
-    };
-
-    const props = defineProps<WorkspaceSelectorProps>();
-
     const emit = defineEmits(['error'])
 
-    const selectedWorkspace = defineModel<string | null>('value');
+    const modelValue = defineModel<string | null>('value');
 
     const options = shallowRef<NaiveUISelectOptionWithColor[]>([]);
 
     const state: AjaxStateInterface = reactive({ ...defaultAjaxState });
-
-    watch(selectedWorkspace, (val) => {
-        if (val) {
-            currentWorkspaceStore.set(val);
-        }
-    });
-
-    const findValidWorkspace = (options: NaiveUISelectOptionWithColor[], stored?: string | null) => {
-        if (!options.length) return null;
-
-        if (stored && options.some(o => o.value === stored)) {
-            return stored;
-        }
-
-        return options[0].value as string;
-    };
 
     const onRefresh = () => {
         Object.assign(state, defaultAjaxState);
@@ -53,10 +27,6 @@
                 disabled: false,
                 color: item.hexColor,
             }));
-            selectedWorkspace.value = findValidWorkspace(
-                options.value,
-                currentWorkspaceStore.currentWorkspaceId
-            );
         }).catch((error: any) => {
             emit("error", { error: error });
         }).finally(() => {
@@ -64,35 +34,14 @@
         });
     };
 
-    const renderLabel = (option: NaiveUISelectOptionWithColor): VNodeChild => {
-        if (option.type === 'group')
-            return `${option.label}(Cool!)`
-        return [
-            h(
-                NIcon,
-                {
-                    color: option.color,
-                    style: {
-                        verticalAlign: '-0.15em',
-                        marginRight: '4px'
-                    }
-                },
-                {
-                    default: () => h(IconSquareFilled)
-                }
-            ),
-            option.label as string
-        ]
-    }
     onMounted(() => {
         onRefresh();
     });
 </script>
 
 <template>
-    <n-select :placeholder="t('Workspace')" v-model:value="selectedWorkspace" :options="options"
-        :render-label="renderLabel" :style="style" :disabled="state.ajaxRunning"
-        :loading="state.ajaxRunning"></n-select>
+    <ColoredIconSelect :placeholder="t('Workspace')" v-model:value="modelValue" :options="options"
+        :filterable="false" />
 </template>
 
 <style lang="css" scoped></style>
