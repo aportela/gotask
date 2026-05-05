a2396ee442d4d9e1b69bb223f836c9e3f2782b38
alex <766f6964+github@gmail.com>
2026-05-05 22:45:26 +0200

    new computed menu

--- vite-vue-ts-frontend/src/layouts/SidebarMenu.vue
+++ vite-vue-ts-frontend/src/layouts/SidebarMenu.vue
@@ -1,34 +1,82 @@
 <script setup lang="ts">
-    import { NMenu } from 'naive-ui';
+    import { ref, nextTick, onMounted } from 'vue';
+    import { NDivider, NMenu } from 'naive-ui';
     import { IconDatabaseStar } from '@tabler/icons-vue';
     import { useRoute } from 'vue-router'
-    import { menuOptions, menuOptionIconSize } from '../types/menu';
+    import { menuOptionIconSize, useMenu } from '../types/menu';
 
-    const route = useRoute()
+    import { useColorSchemeStore } from '../stores/colorScheme';
+    import { useUserSettingsStore } from '../stores/userSettings';
+
+    const colorSchemeStore = useColorSchemeStore();
+    const userSettingsStore = useUserSettingsStore();
+
+    const route = useRoute();
+    const showBrand = true;
+
+    const isMenuCollapsed = ref<boolean>(false);
+
+
+    const { menuOptions, lightTheme, darkTheme, notificationsDisabled, notificationsEnabled } = useMenu();
+
+    const handleMenuSelect = (menuOptionKey: string) => {
+        switch (menuOptionKey) {
+            case "disableNotifications":
+            case "enableNotifications":
+                userSettingsStore.toggleNotifications();
+                nextTick(() => {
+                    notificationsEnabled.value = userSettingsStore.hasNotificationsEnabled;
+                    notificationsDisabled.value = !userSettingsStore.hasNotificationsEnabled;
+                });
+                break;
+            case "switchToLightTheme":
+            case "switchToDarkTheme":
+                colorSchemeStore.toggle();
+                nextTick(() => {
+                    lightTheme.value = colorSchemeStore.light;
+                    darkTheme.value = colorSchemeStore.dark;
+                });
+                break;
+        }
+    }
+
+    onMounted(() => {
+        notificationsEnabled.value = userSettingsStore.hasNotificationsEnabled;
+        notificationsDisabled.value = !userSettingsStore.hasNotificationsEnabled;
+        lightTheme.value = colorSchemeStore.light;
+        darkTheme.value = colorSchemeStore.dark;
+    });
+
 </script>
 
 <template>
-    <div class="brand-container">
-        <IconDatabaseStar :size="32" class="brand-icon" />
-        <span class="brand-name">Doneo</span>
+    <div class="brand-container" v-if="showBrand">
+        <IconDatabaseStar :size="22" class="brand-icon" />
+        <span class="brand-name" v-if="!isMenuCollapsed">Doneo</span>
     </div>
-    <n-menu :collapsed-width="64" :collapsed-icon-size="menuOptionIconSize" :options="menuOptions"
-        :value="route.name as string" accordion />
+    <n-divider class="brand-divider" v-if="showBrand" />
+    <n-menu :collapsed-width="164" :collapsed-icon-size="menuOptionIconSize" :options="menuOptions"
+        :value="route.name as string" accordion :collapsed="isMenuCollapsed" @update:value="handleMenuSelect" />
 </template>
 
 <style lang="css" scoped>
     .brand-container {
         padding-left: 32px;
+        padding-top: 16px;
+        padding-bottom: 16px;
         display: flex;
         align-items: center;
     }
 
     .brand-name {
-        font-weight: bold;
-        font-size: 30px;
+        font-size: --n-font-size;
     }
 
     .brand-icon {
         margin-right: 8px;
+    }
+
+    .brand-divider {
+        margin: 6px 18px;
     }
 </style>
--- vite-vue-ts-frontend/src/types/menu.ts
+++ vite-vue-ts-frontend/src/types/menu.ts
@@ -1,4 +1,4 @@
-import { h, type Component } from "vue";
+import { h, ref, computed, type Component } from "vue";
 import { RouterLink } from "vue-router";
 import { NIcon, NInput, type MenuOption } from "naive-ui";
 import {
@@ -17,6 +17,10 @@ import {
   IconLogout,
   IconId,
   IconSearch,
+  IconBell,
+  IconBellOff,
+  IconMoon,
+  IconSun,
 } from "@tabler/icons-vue";
 
 const menuOptionIconSize = 20;
@@ -33,118 +37,91 @@ const renderIcon = (icon: Component) => {
       );
 };
 
-const menuOptions: MenuOption[] = [
-  {
-    key: "divider-1",
-    type: "divider",
-    props: {
-      style: {
-        marginLeft: "32px",
+export { menuOptionIconSize };
+
+export function useMenu() {
+  const lightTheme = ref(true);
+  const darkTheme = ref(false);
+  const notificationsDisabled = ref(true);
+  const notificationsEnabled = ref(true);
+  const menuOptions = computed(() => {
+    return [
+      {
+        label: () =>
+          h(
+            NInput,
+            {
+              placeholder: "search...",
+              clearable: true,
+            },
+            { default: () => "Overview" },
+          ),
+        key: "search",
+        show: false,
+        icon: renderIcon(IconSearch)(menuOptionIconSize),
       },
-    },
-  },
-  {
-    label: () =>
-      h(
-        NInput,
-        {
-          placeholder: "search...",
-          clearable: true,
-        },
-        { default: () => "Overview" },
-      ),
-    key: "search",
-    show: false,
-    icon: renderIcon(IconSearch)(menuOptionIconSize),
-  },
-  {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: "home",
-            params: {},
-          },
-        },
-        { default: () => "Overview" },
-      ),
-    key: "home",
-    icon: renderIcon(IconPresentation)(menuOptionIconSize),
-  },
-  {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: "projects",
-            params: {},
-          },
-        },
-        { default: () => "Projects" },
-      ),
-    key: "projects",
-    icon: renderIcon(IconSitemap)(menuOptionIconSize),
-  },
-  {
-    label: "Tasks",
-    key: "tasks",
-    disabled: true,
-    icon: renderIcon(IconBug)(menuOptionIconSize),
-  },
-  {
-    label: "Reports",
-    key: "reports",
-    disabled: true,
-    icon: renderIcon(IconFileAnalytics)(menuOptionIconSize),
-  },
-  {
-    label: "Charts",
-    key: "charts",
-    disabled: true,
-    icon: renderIcon(IconChartHistogram)(menuOptionIconSize),
-  },
-  {
-    key: "divider-2",
-    type: "divider",
-    show: false,
-    props: {
-      style: {
-        marginLeft: "32px",
-      },
-    },
-  },
-  {
-    label: "Settings",
-    key: "settings",
-    show: true,
-    icon: renderIcon(IconSettings)(menuOptionIconSize),
-    children: [
       {
         label: () =>
           h(
             RouterLink,
             {
               to: {
-                name: "manageUsers",
+                name: "home",
                 params: {},
               },
             },
-            { default: () => "Users" },
+            { default: () => "Overview" },
           ),
-        key: "manageUsers",
-        icon: renderIcon(IconUsers)(menuOptionIconSize),
+        key: "home",
+        icon: renderIcon(IconPresentation)(menuOptionIconSize),
       },
       {
-        label: "Roles",
-        key: "roles",
+        label: () =>
+          h(
+            RouterLink,
+            {
+              to: {
+                name: "projects",
+                params: {},
+              },
+            },
+            { default: () => "Projects" },
+          ),
+        key: "projects",
+        icon: renderIcon(IconSitemap)(menuOptionIconSize),
+      },
+      {
+        label: "Tasks",
+        key: "tasks",
         disabled: true,
-        icon: renderIcon(IconUserCheck)(menuOptionIconSize),
+        icon: renderIcon(IconBug)(menuOptionIconSize),
       },
       {
-        label: "Project",
-        key: "projectSettings",
+        label: "Reports",
+        key: "reports",
+        disabled: true,
+        icon: renderIcon(IconFileAnalytics)(menuOptionIconSize),
+      },
+      {
+        label: "Charts",
+        key: "charts",
+        disabled: true,
+        icon: renderIcon(IconChartHistogram)(menuOptionIconSize),
+      },
+      {
+        key: "divider-2",
+        type: "divider",
+        show: false,
+        props: {
+          style: {
+            marginLeft: "32px",
+          },
+        },
+      },
+      {
+        label: "Settings",
+        key: "settings",
+        show: true,
         icon: renderIcon(IconSettings)(menuOptionIconSize),
         children: [
           {
@@ -153,76 +130,133 @@ const menuOptions: MenuOption[] = [
                 RouterLink,
                 {
                   to: {
-                    name: "manageProjectTypes",
+                    name: "manageUsers",
                     params: {},
                   },
                 },
-                { default: () => "Type" },
+                { default: () => "Users" },
               ),
-            key: "manageProjectTypes",
-            icon: renderIcon(IconBookmark)(menuOptionIconSize),
+            key: "manageUsers",
+            icon: renderIcon(IconUsers)(menuOptionIconSize),
           },
           {
-            label: () =>
-              h(
-                RouterLink,
-                {
-                  to: {
-                    name: "manageProjectPriorities",
-                    params: {},
-                  },
-                },
-                { default: () => "Priority" },
-              ),
-            key: "manageProjectPriorities",
-            icon: renderIcon(IconFlagBolt)(menuOptionIconSize),
+            label: "Roles",
+            key: "roles",
+            disabled: true,
+            icon: renderIcon(IconUserCheck)(menuOptionIconSize),
           },
           {
-            label: () =>
-              h(
-                RouterLink,
-                {
-                  to: {
-                    name: "manageProjectStatuses",
-                    params: {},
-                  },
-                },
-                { default: () => "Status" },
-              ),
-            key: "manageProjectStatuses",
-            icon: renderIcon(IconAdjustmentsBolt)(menuOptionIconSize),
+            label: "Project",
+            key: "projectSettings",
+            icon: renderIcon(IconSettings)(menuOptionIconSize),
+            children: [
+              {
+                label: () =>
+                  h(
+                    RouterLink,
+                    {
+                      to: {
+                        name: "manageProjectTypes",
+                        params: {},
+                      },
+                    },
+                    { default: () => "Type" },
+                  ),
+                key: "manageProjectTypes",
+                icon: renderIcon(IconBookmark)(menuOptionIconSize),
+              },
+              {
+                label: () =>
+                  h(
+                    RouterLink,
+                    {
+                      to: {
+                        name: "manageProjectPriorities",
+                        params: {},
+                      },
+                    },
+                    { default: () => "Priority" },
+                  ),
+                key: "manageProjectPriorities",
+                icon: renderIcon(IconFlagBolt)(menuOptionIconSize),
+              },
+              {
+                label: () =>
+                  h(
+                    RouterLink,
+                    {
+                      to: {
+                        name: "manageProjectStatuses",
+                        params: {},
+                      },
+                    },
+                    { default: () => "Status" },
+                  ),
+                key: "manageProjectStatuses",
+                icon: renderIcon(IconAdjustmentsBolt)(menuOptionIconSize),
+              },
+            ],
           },
         ],
       },
-    ],
-  },
-  {
-    key: "divider-3",
-    type: "divider",
-    props: {
-      style: {
-        marginLeft: "32px",
-      },
-    },
-  },
-  {
-    label: "John Doe",
-    key: "myuser",
-    icon: renderIcon(IconUserCircle)(menuOptionIconSize),
-    children: [
       {
-        label: "Profile",
-        key: "profile",
-        disabled: true,
-        icon: renderIcon(IconId)(menuOptionIconSize),
+        key: "divider-3",
+        type: "divider",
+        props: {
+          style: {
+            marginLeft: "32px",
+          },
+        },
       },
       {
-        label: "Logout",
-        key: "signout",
-        icon: renderIcon(IconLogout)(menuOptionIconSize),
+        label: "John Doe",
+        key: "myuser",
+        icon: renderIcon(IconUserCircle)(menuOptionIconSize),
+        children: [
+          {
+            label: "Disable notifications",
+            key: "disableNotifications",
+            show: notificationsDisabled.value,
+            icon: renderIcon(IconBellOff)(menuOptionIconSize),
+          },
+          {
+            label: "Enable notifications",
+            key: "enableNotifications",
+            show: notificationsEnabled.value,
+            icon: renderIcon(IconBell)(menuOptionIconSize),
+          },
+          {
+            label: "Light theme",
+            key: "switchToLightTheme",
+            show: darkTheme.value,
+            icon: renderIcon(IconSun)(menuOptionIconSize),
+          },
+          {
+            label: "Dark theme",
+            key: "switchToDarkTheme",
+            show: lightTheme.value,
+            icon: renderIcon(IconMoon)(menuOptionIconSize),
+          },
+          {
+            label: "Profile",
+            key: "profile",
+            disabled: true,
+            icon: renderIcon(IconId)(menuOptionIconSize),
+          },
+          {
+            label: "Logout",
+            key: "signout",
+            icon: renderIcon(IconLogout)(menuOptionIconSize),
+          },
+        ],
       },
-    ],
-  },
-];
-
-export { menuOptions, menuOptionIconSize };
+    ] as MenuOption[];
+  });
+  return {
+    menuOptions,
+    lightTheme,
+    darkTheme,
+    notificationsDisabled,
+    notificationsEnabled,
+  };
+}
