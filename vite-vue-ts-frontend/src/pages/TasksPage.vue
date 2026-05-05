c1a96d1dd96c7f533b5ef56da360272d35221af4
alex <766f6964+github@gmail.com>
2026-05-06 01:20:52 +0200

    added dummy tasks page

--- vite-vue-ts-frontend/src/pages/TasksPage.vue
+++ vite-vue-ts-frontend/src/pages/TasksPage.vue
@@ -1,8 +1,51 @@
 <script setup lang="ts">
+    import { NTabs, NTabPane, NIcon } from 'naive-ui';
+    import { IconList, IconLayoutKanban, IconCalendarWeek } from '@tabler/icons-vue';
 </script>
 
 <template>
-    TASKS
+
+
+    <n-tabs>
+        <n-tab-pane name="List" tab="List" type="segment" animated>
+            <template #tab>
+                <n-icon size="16" class="tab-icon">
+                    <IconList />
+                </n-icon>
+                List
+            </template>
+            <template #default>
+                LIST
+            </template>
+        </n-tab-pane>
+        <n-tab-pane name="Kanban" tab="Kanban">
+            <template #tab>
+                <n-icon size="16" class="tab-icon">
+                    <IconLayoutKanban />
+                </n-icon>
+                Kanban
+            </template>
+            <template #default>
+                KANBAN
+            </template>
+        </n-tab-pane>
+        <n-tab-pane name="Calendar" tab="Calendar">
+            <template #tab>
+                <n-icon size="16" class="tab-icon">
+                    <IconCalendarWeek />
+                </n-icon>
+                Calendar
+            </template>
+            <template #default>
+                CALENDAR
+            </template>
+        </n-tab-pane>
+    </n-tabs>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+
+    .tab-icon {
+        margin-right: 4px;
+    }
+</style>
