987e1ff210da4be9bd3efbec90320a5ea29f6a10
alex <766f6964+github@gmail.com>
2026-05-10 15:39:35 +0200

    title removed & changed slots

--- vite-vue-ts-frontend/src/components/custom/ManageTable.vue
+++ vite-vue-ts-frontend/src/components/custom/ManageTable.vue
@@ -1,49 +1,26 @@
 <script setup lang="ts">
-    import { type CSSProperties } from 'vue';
     import { NTable, type TableSize } from 'naive-ui';
 
-
     interface ManageTableProps {
-        size: TableSize;
-        title: string;
-        style?: string | CSSProperties;
+        size?: TableSize;
+        striped?: boolean;
     };
 
     const props = defineProps<ManageTableProps>();
 </script>
 
 <template>
-    <div class="table-header">
-        <div>
-            <span class="table-caption-title">{{ title }}</span>
-        </div>
-        <div>
-            <slot name="caption-extra" />
-        </div>
-    </div>
-    <n-table :size="size" :style="style">
-        <slot name="thead">
-            <thead></thead>
-        </slot>
-        <slot name="tbody">
-            <tbody></tbody>
-        </slot>
-        <slot name="tfoot">
-            <tfoot></tfoot>
-        </slot>
+    <n-table :size="size" :striped="striped">
+        <thead>
+            <slot name="thead" />
+        </thead>
+        <tbody>
+            <slot name="tbody" />
+        </tbody>
+        <tfoot>
+            <slot name="tfoot" />
+        </tfoot>
     </n-table>
 </template>
 
-<style lang="css" scoped>
-    .table-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 4px;
-    }
-
-    .table-caption-title {
-        font-weight: 700;
-        font-size: large;
-    }
-</style>
+<style lang="css" scoped></style>
