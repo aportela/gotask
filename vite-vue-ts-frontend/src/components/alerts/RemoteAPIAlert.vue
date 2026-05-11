05afa1b82574da07993fcf64eb2e7bfdcfc59b0a
alex <766f6964+github@gmail.com>
2026-05-11 17:51:31 +0200

    allow optional props

--- vite-vue-ts-frontend/src/components/alerts/RemoteAPIAlert.vue
+++ vite-vue-ts-frontend/src/components/alerts/RemoteAPIAlert.vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
     import { NAlert } from 'naive-ui'
 
-    type AlertType = "default" | "info" | "warning" | "error" | "success" | undefined;
+    type AlertType = "default" | "info" | "warning" | "error" | "success";
 
     interface RemoteAPIAlertProps {
-        title: string,
-        type: AlertType,
-        message: string
-    };
+        title?: string
+        type?: AlertType
+        message?: string
+    }
 
-    withDefaults(defineProps<RemoteAPIAlertProps>(), {
+    const props = withDefaults(defineProps<RemoteAPIAlertProps>(), {
         title: "",
-        type: undefined,
+        type: "default",
         message: "",
-    });
+    })
+
 </script>
 
 <template>
     <n-alert :title="title" :type="type">
-        <span v-html="message.split('\n').map(line => `${line}<br />`).join('')"></span>
+        <pre class="alert-message">{{ message }}</pre>
     </n-alert>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    .alert-message {
+        white-space: pre-wrap;
+        margin: 0;
+    }
+</style>
