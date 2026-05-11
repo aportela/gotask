13b93f7e77c7bb6a1ebfb203871da61209494fe1
alex <766f6964+github@gmail.com>
2026-05-11 14:14:10 +0200

    new pager/order structs

--- golang-backend/internal/handlers/dto.go
+++ golang-backend/internal/handlers/dto.go
@@ -2,3 +2,32 @@ package handlers
 
 type EmptyResponse struct {
 }
+
+type PagerRequest struct {
+	CurrentPage int `json:"currentPage"`
+	ResultsPage int `json:"resultsPage"`
+}
+
+type PagerResponse struct {
+	Enabled      bool `json:"enabled"`
+	CurrentPage  int  `json:"currentPage"`
+	ResultsPage  int  `json:"resultsPage"`
+	TotalPages   int  `json:"totalPages"`
+	TotalResults int  `json:"totalResults"`
+}
+
+const (
+	SortAsc  SortOrder = "ASC"
+	SortDesc SortOrder = "DESC"
+)
+
+type SortOrder string
+
+func (o SortOrder) IsValid() bool {
+	return o == SortAsc || o == SortDesc
+}
+
+type OrderRequest struct {
+	Field string    `json:"field"`
+	Sort  SortOrder `json:"sort"`
+}
