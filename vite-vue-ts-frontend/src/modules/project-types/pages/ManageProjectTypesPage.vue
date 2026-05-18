a032c41a6b2d795844c213d6ec00aa769900bedc
alex <766f6964+github@gmail.com>
2026-05-18 21:56:19 +0200

    disable pager by default for project types

--- vite-vue-ts-frontend/src/modules/project-types/pages/ManageProjectTypesPage.vue
+++ vite-vue-ts-frontend/src/modules/project-types/pages/ManageProjectTypesPage.vue
@@ -33,7 +33,7 @@
     const nameFilter = ref<string>("");
 
     const currentPage = ref(1);
-    const pageSize = ref(10);
+    const pageSize = ref(0);
     const totalResults = ref(0);
     const totalPages = ref(0);
 
